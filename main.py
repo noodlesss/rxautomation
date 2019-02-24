b9512c8522c484fe97dc0569405c49b311d98213
noodlesss <nuran.af@gmail.com>
2019-02-24 02:26:47 +0100

    asd

--- main.py
+++ main.py
@@ -49,79 +49,64 @@ def api2():
     }
     return cluster_vars
 
-def callback(ch, method, properties, body):
+# Rabbitmq queue callback
+def con_callback(ch, method, properties, body):
     body = json.loads(body)
     botik.bot.sendMessage(chat_id, body)
     logging.info('task: %s\n result: %s') %(body['task'], body['result'])
 
-class Botik(object):
-    def __init__(self, token, api3_vars):
-        self.token = token
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue='deployer')
-        self.api3_vars = api3_vars
-        self.bot = telepot.Bot(self.token)
-        MessageLoop(self.bot, {'chat': self.handler,
-                  'callback_query': self.callback}).run_as_thread()
-        logging.info('bot started listening')
 
+        
 
-    def handler(self, msg):
-        print(msg['text'])
+# Telegram handler
+def handler(msg):
+    print(msg['text'])
 
-
-    def callback(self, msg):
-        print(msg)
-        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-        logging.info('Callback Query: %s' %(query_data))
-        if query_data == 'start':    ## checking call back data, and starting cluster status check process
-            message = {'action':'checkcluster', 'data':self.api3_vars}
-            self.channel.basic_publish(exchange='',
-                      routing_key='deployer',
-                      body=json.dumps(message))
-            self.bot.answerCallbackQuery(query_id, text='action %s send to queue' %message['action'])
-
-
-def main():
-    # Log object
-    logging.basicConfig(filename='telegram_ctr.log', format='%(asctime)s:%(levelname)s:%(message)s', level=logging.INFO)
-    #init vars
-    print(os.environ.keys)
-    network_vars = network()
-    pc_vars = pc()
-    api3_vars = api3()
-    api2_vars = api2()
-    deploy_ctr_ip = os.environ['deploy_ctr_ip']
-    #telegram token for bot
-    token = '168023423:AAFa-zgvR_8Xw8iRuyG2QxIyQdNCwMqDHA8'
-    logging.info('Token: %s' %token)
-    #initialize bot
-    botik = Botik(token, api3_vars)
-    chat_id = '165756165'
-    logging.info('container started')
-    botik.bot.sendMessage(chat_id, 'container started')
-    logging.info('bot started listening')
-    # start checking process?
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='start cluster_ip: %s' %api3_vars['cluster_ip'], callback_data='start')],
-               ])
-    botik.bot.sendMessage(chat_id, 'a', reply_markup=keyboard)
-    # while loop to keep bot listening..
-    connection_reply = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel_reply = connection_reply.channel()
-    channel_reply.queue_declare(queue='reply')
-    channel_reply.basic_consume(callback, queue='reply', no_ack=True)
-    logging.info('consumer started. listening..')
-    channel_reply.start_consuming()
-
-    
+# Telegram callback
+def callback(msg):
+    print(msg)
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    logging.info('Callback Query: %s' %(query_data))
+    if query_data == 'start':    ## checking call back data, and starting cluster status check process
+        message = {'action':'checkcluster', 'data':api3_vars}
+        channel.basic_publish(exchange='',
+                  routing_key='deployer',
+                  body=json.dumps(message))
+        bot.answerCallbackQuery(query_id, text='action %s send to queue' %message['action'])
 
 
 
-
-
-if __name__ == '__main__':
-    main()
+# Log object
+logging.basicConfig(filename='main_ctr.log', format='%(asctime)s:%(levelname)s:%(message)s', level=logging.INFO)
+logging.info('container started')
+#init vars
+print(os.environ.keys)
+network_vars = network()
+pc_vars = pc()
+api3_vars = api3()
+api2_vars = api2()
+#telegram token for bot
+token = '168023423:AAFa-zgvR_8Xw8iRuyG2QxIyQdNCwMqDHA8'
+chat_id = '165756165'
+logging.info('Token: %s' %token)
+  #initialize bot
+bot = telepot.Bot(token)
+MessageLoop(bot, {'chat': handler,
+                  'callback_query': callback}).run_as_thread()
+logging.info('bot started listening')
+botik.bot.sendMessage(chat_id, 'container started')
+logging.info('bot started listening')
+# start checking process?
+keyboard = InlineKeyboardMarkup(inline_keyboard=[
+               [InlineKeyboardButton(text='start cluster_ip: %s' %api3_vars['cluster_ip'], callback_data='start')],
+           ])
+bot.sendMessage(chat_id, 'a', reply_markup=keyboard)
+# while loop to keep bot listening..
+connection_reply = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel_reply = connection_reply.channel()
+channel_reply.queue_declare(queue='reply')
+channel_reply.basic_consume(con_callback, queue='reply', no_ack=True)
+logging.info('consumer started. listening..')
+channel_reply.start_consuming()
 
 
