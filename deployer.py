b9ce66bda4cbc4d5c911cfc763d65e6da38a77f3
noodlesss <nuran.af@gmail.com>
2019-02-25 13:51:05 +0100

    mods done

--- deployer.py
+++ deployer.py
@@ -12,10 +12,6 @@ from telepot.delegate import (
 #logging config
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.INFO)
 
-#bot config
-token = '168023423:AAFa-zgvR_8Xw8iRuyG2QxIyQdNCwMqDHA8'
-bot = telepot.Bot(token)
-chat_id = '165756165'
 
 
 
@@ -44,7 +40,7 @@ def check_cluster_status(base_url, username, password):
             else:
                 logging.info('waiting...')
                 time.sleep(900)
-        except requests.exceptions.ConnectTimeout as e:
+        except (requests.exceptions.ConnectTimeout, urllib3.exceptions.MaxRetryError) as e:
             logging.info('waiting')
             time.sleep(900)
 
@@ -53,21 +49,24 @@ def callback(ch, method, properties, body):
     body = json.loads(body)
     logging.info("[x] Received %r" % body)
     action = body['action']
-    if action == 'checkcluster':
+    if action == 'checkcluster':    # check cluster  status
         cluster_status = check_cluster_status(body['apidata']['base_url'], body['apidata']['username'], body['apidata']['password'])
         if cluster_status == True:
             publisher({'task':'cluster_status', 'result': 'cluster up'})
         else:
             publisher({'task':'cluster_status', 'result': 'check failed'})
-    elif action == 'create_network':
+    elif action == 'create_network':  # create network 
         create_network = network_create_v2.netcreate(body)
-        publisher({'task': 'create_network', 'result': create_network.status_code})
+        publisher({'task': 'create_network', 'result': create_network.status_code}) # status codee 201 means task accepted. 
+    elif action == 'deploy_pc':  # deploy pc
+        pc = deploy_pc.
     logging.info(" [x] Done")
 
 
+# init Rabbitmq queue and listen for commands.
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='deployer')
+channel.queue_declare(queue='deployer')    # deployer queue for actions to run on cluster
 channel.basic_consume(callback, queue='deployer', no_ack=True)
 logging.info('consumer started. listening..')
 channel.start_consuming()
